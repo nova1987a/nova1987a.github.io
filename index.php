b44a112ff94832562a1a5c1073b626108bcaf8c2
nova1987a <118440807+nova1987a@users.noreply.github.com>
2023-01-03 17:17:22 +0800

    Update index.php

--- index.php
+++ index.php
@@ -5,9 +5,9 @@
             <div class="row g-2">
                 <div class="col-8 gv-3 px-4">
                     <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
-                       <h2>Top Rated Places</h2>
-                        <p>   Sed sit amet augue congue, tincidunt ligula pretium, acumin dolor ligula dolor condimentum leo. 
-                              Proin vel diam sit amet orci tincidunt commodo. Nulla acumin ligula, dolor sit buncha elementum ligula.</p>
+                        <h2 class="alert alert-info text-center">Top Rated Places</h2>
+                        <p>   Sed sit amet augue congue, tincidunt ligula pretium, acumin dolor ligula dolor condimentum leo.
+                        Proin vel diam sit amet orci tincidunt commodo. Nulla acumin ligula, dolor sit buncha elementum ligula.</p>
                         <div class="carousel-inner">
                             <!-- image slide from folder w/ php -->
                             <?php
@@ -41,10 +41,10 @@
                 </div>
                 <!-- Sidebar section-->
                 <div class="col-4 gv-3 px-4">
-                    <h4>News Feed</h4>
+                    <h4 class="alert alert-primary text-center">News Feed</h4>
                     <?php require 'functions/rss.php';
-                    $rss = 'data/recent-opeds.rss';
-                    show_title('data/rr.rss');
+                    $rss = 'data/motogp.rss';
+                    show_title($rss);
                     ?>
                 </div>
             </div>
