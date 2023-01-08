c27417e654756c45df4588dcbaf7de1731d2de80
nova1987a <118440807+nova1987a@users.noreply.github.com>
2023-01-08 14:34:09 +0800

    Add files via upload
    Add new file for manage page

---
+++ functions/manage_header.php
@@ -0,0 +1,50 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <!-- refresh in 30 sec. and logout if idle -->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="refresh" content="900; url=../logout.php">
+    <title>One page demo</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/styles.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+    
+    </head>
+    <body>
+        <nav class="navbar navbar-expand-lg navbar-dark " style="background-color: #35396E;" aria-label="Ninth navbar example">
+            <div class="container-xl">
+                <a class="navbar-brand" href="https://ab1.novatw.net">Hobby page</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" aria-expanded="false" aria-label="Toggle navigation">
+                  <span class="navbar-toggler-icon"></span>
+                </button>
+            
+                <div class="collapse navbar-collapse" id="navbarsExample07XL">
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="https://ab1.novatw.net">Home</a>
+                        </li>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" data-bs-toggle="dropdown" aria-expanded="false">咖啡．旅遊</a>
+                            <ul class="dropdown-menu">
+                                <li><a class="dropdown-item" href="https://www.coffeeology.com.tw">咖啡學</a></li>
+                                <li><a class="dropdown-item" href="https://khh.travel/">高雄旅遊網</a></li>
+                                <li><a class="dropdown-item" href="https://mys.coffee/">My's Cafe</a></li>
+                            </ul>
+                        </li>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" data-bs-toggle="dropdown" aria-expanded="false">管理頁面</a>
+                            <ul class="dropdown-menu">
+                                <li><a class="dropdown-item" href="https://ab1.novatw.net/manage.php">管理</a></li>
+                                <li><a class="dropdown-item" href="https://ab1.novatw.net/login.php">登入</a></li>
+                                <li><a class="dropdown-item" href="https://ab1.novatw.net/signup.php">註冊使用者</a></li>
+                                <li><a class="dropdown-item" href="https://ab1.novatw.net/functions/reset-passwd.php">重設使用者密碼</a></li>
+                            </ul>
+                        </li>
+                </div>
+                    <form class="d-flex">
+                        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                        <button class="btn btn-outline-success" type="submit">Search</button>
+                    </form>
+            </div>
+        </nav>
